dfe26b41f9a36c73774bff6283e47faad55a6712
TharunJavaji05 <tharunjavaji0501@gmail.com>
2025-04-14 23:32:16 -0700

    Update server.js

--- Lab-7/server/server.js
+++ Lab-7/server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Middleware to parse form data (URL encoded form data)
+// Middleware to parse JSON and URL-encoded form data
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "public" folder (located at the root of your project)
